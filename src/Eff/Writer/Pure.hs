e98a6d1c19d77bf2903466f8338e9c372c150dda
Sandy Maguire <sandy@sandymaguire.me>
2017-05-12 17:32:10 -0600

    ignore writer

--- src/Eff/Writer/Pure.hs
+++ src/Eff/Writer/Pure.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE GADTs         #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Eff.Writer.Pure
   ( module Eff.Writer
@@ -15,3 +16,8 @@ runWriter :: Monoid o => Eff (Writer o ': r) a -> Eff r (a, o)
 runWriter = handleRelay (\x -> return (x, mempty))
                   (\ (Writer o) k -> k () >>= \ (x,l) -> return (x,o `mappend` l))
 
+ignoreWriter :: forall o r a. Eff (Writer o ': r) a -> Eff r a
+ignoreWriter = handleRelay pure bind
+  where
+    bind :: forall x. Writer o x -> (x -> Eff r a) -> Eff r a
+    bind (Writer _) arr = arr ()
